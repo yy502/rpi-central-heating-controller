2329edbbab381e7c5366a57628a52a045ff8bbe4
Yi Yu <yi.yu@ocado.com>
2017-02-21 13:16:52 +0000

    simplified GPIO control scripts

--- setup_gpio.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CH="2"
-HW="3"
-
-/usr/local/bin/gpio mode $CH output
-/usr/local/bin/gpio mode $HW output
-
-gpio readall | grep "GPIO. [2,3]" | awk '{print $4,$9,$11}'
--- switch.sh
+++ switch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# a wrapper script to switch the relays on/off via GPIOs
+# execute 'switch.sh setup' once at boot to set the GPIOs in output mode
+
 CH="2"
 HW="3"
 
@@ -17,7 +20,14 @@ if [ "$1" == "ch" ] || [ "$1" == "hw" ] && [ "$2" == "on" ] || [ "$2" == "off" ]
     
     /usr/local/bin/gpio write $TYPE $MODE
     gpio readall | grep "GPIO. $TYPE" | awk '{print $4,$9,$11}'
+
+elif [ "$1" == "setup" ]; then
+    /usr/local/bin/gpio mode $CH output
+    /usr/local/bin/gpio mode $HW output
+    gpio readall | grep "GPIO. [2,3]" | awk '{print $4,$9,$11}'
+
 else
-    echo "Usage: switch.sh <ch|hw> <on|off>"
+    echo "Usage: switch.sh [<ch|hw> <on|off>] | [setup]"
+
 fi
 
