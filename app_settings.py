be439b0c7f69a5e68e49648a516b7f71868855bf
Yi Yu <yi.yu@ocado.com>
2017-02-28 16:02:05 +0000

    better settings management

--- app_settings.py
+++ app_settings.py
@@ -4,6 +4,7 @@ from logger import *
 import json
 import sys
 import os
+import time
 
 # relay wiringPi pins
 CH = 2
@@ -16,24 +17,40 @@ LOCAL = 24
 REMOTE = "http://192.168.0.41"
 
 # settings path including file name
-SETTINGS_DIR = "settings"
-DEFAULT = os.path.join(sys.path[0], SETTINGS_DIR, "default.json")
-DAILY = os.path.join(sys.path[0], SETTINGS_DIR, "20170301.json")  # file name will be today's date
+SETTINGS_DIR = os.path.join(sys.path[0], "settings")
+DEFAULT = os.path.join(SETTINGS_DIR, "default.json")
+TODAY = time.strftime("%Y%m%d",time.localtime(time.time()))  # YYYYMMDD
+DAILY = os.path.join(SETTINGS_DIR, "%s.json" % TODAY)
 
 settings = {}
 
-try:
-    with open(DEFAULT, 'r') as f:
-        settings = json.load(f)
-        logging.info("Loaded %s", DEFAULT)
-except:
-    logging.exception("Unable to load default settings: %s", DEFAULT)
-    raise
+def load_default():
+    try:
+        with open(DEFAULT, 'r') as f:
+            settings = json.load(f)
+            logging.info("Loaded %s", DEFAULT)
+    except:
+        logging.exception("Unable to load default settings: %s", DEFAULT)
+        raise
 
-try:
-    with open(DAILY, 'r') as f:
-        settings.update(json.load(f))
-        logging.info("Loaded %s", DAILY)
-except:
-    logging.exception("Daily settings not found: %s", DAILY)
-    logging.info("Using default settings only.")
+def load_daily():
+    try:
+        with open(DAILY, 'r') as f:
+            settings.update(json.load(f))
+            logging.info("Loaded %s", DAILY)
+    except IOError:
+        logging.warn("Daily settings not found: %s", DAILY)
+        generate_daily_settings()
+
+def generate_daily_settings():
+    # take defaults and generate today's settings json file
+    try:
+        logging.info("Daily settings generated: %s", DAILY)
+    except:
+        logging.exception("Unable to generate daily settings: %s", DEFAULT)
+        # kick off beeper!
+
+
+# populate settings dictionary
+load_default()
+load_daily()
