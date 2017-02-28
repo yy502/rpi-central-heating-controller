2f2de1cb7efdda7c976886c4034198bfc2d97ba3
Yi Yu <yi.yu@ocado.com>
2017-02-28 11:55:39 +0000

    fixed log path

--- logger.py
+++ logger.py
@@ -1,7 +1,8 @@
 import logging
 import sys
+import os
 
-LOGNAME = "%s/control.log" % sys.path[0]
+LOGNAME = os.path.join(sys.path[0], "control.log")
 
 logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s] %(message)s")
 rootLogger = logging.getLogger()
