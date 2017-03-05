d6f0980178fb42b6c3e7947c90c6240fbb147446
Yi Yu <yi@cs.st-andrews.ac.uk>
2017-03-05 21:24:37 +0000

    WIP

--- app_settings.py
+++ app_settings.py
@@ -1,10 +1,12 @@
 # merge base and override settings into one settings dictionary
 
 from logger import *
+from beeper import beep
 import json
 import sys
 import os
 import time
+import datetime
 
 # relay wiringPi pins
 CH = 2
@@ -22,58 +24,79 @@ BEEPS = 8
 
 # settings path including file name
 SETTINGS_DIR = os.path.join(sys.path[0], "settings")
-DEFAULT = os.path.join(SETTINGS_DIR, "default.json")
-TODAY = time.strftime("%y%m%d",time.localtime(time.time()))  # YYMMDD
-DAILY = os.path.join(SETTINGS_DIR, "%s.json" % TODAY)
+DEFAULT_FNAME = os.path.join(SETTINGS_DIR, "default.json")
+TODAY_DATE = time.strftime("%y%m%d",time.localtime(time.time()))  # YYMMDD
+TODAY_WEEKDAY = time.strftime("%a",time.localtime(time.time())).lower()
 
+# settings validation
+WEEKDAYS = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+MODES = ["auto", "off"]
 
 settings = {}
 
 
-def parse_default(default_settings):
-    """ Converts default weekday settings to daily settings.
-    Returns daily settings dictionary.
-    """
-    return {"place_holder": 1}
-
-
-def load_default():
-    global settings
+def load_default_for_date(date):
+    """ Load and parse default weekday settings, return a day's settings """
+    weekday = datetime.datetime.strptime(date, '%y%m%d').strftime('%a').lower()
     try:
-        with open(DEFAULT, 'r') as f:
-            settings = parse_default(json.load(f))
-            logging.info("Loaded %s", DEFAULT)
+        with open(DEFAULT_FNAME, 'r') as f:
+            defaults = json.load(f)
+            for ctrl_type in ["ch", "hw"]:
+                for key in defaults[ctrl_type].keys():
+                    if weekday in key:
+                        defaults[ctrl_type] = defaults[ctrl_type][key]
+                        break
+                # by the end of the inner loop, today's settings for ctrl_type
+                # should have been brought up by one level, directly under ctrl_type
+                # now validate it as below
+                first_key = next(iter(defaults[ctrl_type]))
+                print defaults[ctrl_type][first_key]
+                if type(defaults[ctrl_type][first_key]) is not str:
+                    # today's weekday is not found in default weekday settings
+                    logging.error("Today's (%s) setting not found in default settings.", weekday)
+                    logging.warn("Turning CH and HW off for today due to lack of settings.")
+                    # turn off everything
+                    beep()
+                    sys.exit()
+        logging.info("Loaded default settings for %s", date)
+        return defaults
     except:
-        logging.exception("Unable to load default settings: %s", DEFAULT)
+        logging.exception("Unable to load default settings: %s", DEFAULT_FNAME)
         raise
 
 
-def save_daily(dict=None, date=TODAY):
+def save_day_settings(dict=None, date=TODAY_DATE):
+    """ Save given date's settings to a json file.
+
+    dict:   settings dictionary
+    date:   date string in YYMMDD format
+    """
     with open(os.path.join(SETTINGS_DIR, "%s.json" % date), 'w') as f:
         f.write(json.dumps(dict, indent=4, sort_keys=True))
 
 
-def generate_daily_settings():
-    # take defaults and generate today's settings json file
+def generate_day_settings(date):
+    """ take defaults and generate given date's settings json file """
     try:
-        load_default()
-        save_daily(dict=settings)
-        logging.info("Daily settings generated and loaded: %s", DAILY)
+        save_day_settings(dict=load_default(date=date), date=date)
+        logging.info("Settings for %s generated.", date)
     except:
-        logging.exception("Unable to generate daily settings: %s", DEFAULT)
+        logging.exception("Unable to generate settings for %s", date)
         # this is bad... kick off beeper and stop here!
+        beep()
         raise
 
 
-def load_daily():
+def load_by_date(date=TODAY_DATE):
     global settings
+    fname = os.path.join(SETTINGS_DIR, date+".json")
     try:
-        with open(DAILY, 'r') as f:
-            settings.update(json.load(f))
-            logging.info("Loaded %s", DAILY)
+        with open(fname, 'r') as f:
+            settings = json.load(f)
+            logging.info("Loaded %s", fname)
     except IOError:
-        logging.warn("Daily settings not found: %s", DAILY)
-        generate_daily_settings()
+        logging.warn("Settings for %s not found.", date)
+        generate_day_settings(date)
 
 
-load_daily()
+load_by_date()
